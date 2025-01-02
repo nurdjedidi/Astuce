0ca78f7d06fc8324110925ba476e892b9784d89d
Nûr Djedidi <djedidinur@gmail.com>
2025-01-02 11:37:39 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>  
+    <div>  
+      <label for="name">Votre nom :</label>  
+      <input v-model="name" id="name" type="text" />  
+      <p>Bonjour, {{ name }} ! 👋</p>  
+    </div>  
+  </template>  
+  
+  <script>  
+  export default {  
+    data() {  
+      return {  
+        name: ''  
+      };  
+    }  
+  };  
+  </script>  
+  
